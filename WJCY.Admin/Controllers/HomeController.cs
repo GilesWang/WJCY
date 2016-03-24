cbc6bc040fe7d482129807eebded3d8f66643954
GilesWang <wfagui@gmail.com>
2016-03-24 23:24:02 +0800

    test

--- WJCY.Admin/Controllers/HomeController.cs
+++ WJCY.Admin/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace WJCY.Admin.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "My application description page.";
 
             return View();
         }
